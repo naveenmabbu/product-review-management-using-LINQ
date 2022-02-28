8a731886c2fb7c087671bf0f9c2f87162710d869
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-02-28 23:08:15 +0530

    Add Retrieve records for a specified condition

---
+++ Management.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagementWithLinq
+{
+    class ProductReviewRepository
+    {
+        /// <summary>
+        /// UC 2:
+        /// Returns the top three products with maximum rating
+        /// </summary>
+        /// <param name="productReviewList"></param>
+        public void GetTopThreeProducts(List<ProductReview> productReviewList)
+        {
+            var recordedData = (from products in productReviewList
+                                orderby products.Rating descending
+                                select products).Take(3);
+            foreach (var productReview in recordedData)
+            {
+                Console.WriteLine("Product ID: " + productReview.ProductID + " User ID: " + productReview.UserID + " Rating: " + productReview.Rating + " Review: " + productReview.Review);
+            }
+        }
+        /// <summary>
+        ///UC 3
+        ///Get products for a specified codition
+        /// </summary>
+        /// <param name="list"></param>
+        public void RetrieveProductsForCondition(List<ProductReview> list)
+        {
+            var recordedData = (list.Where(r => r.Rating > 3 && r.ProductID == 1 || r.ProductID == 4 || r.ProductID == 9));
+            foreach (var productReview in recordedData)
+            {
+                Console.WriteLine("Product ID: " + productReview.ProductID + "User ID: " + productReview.UserID + "Rating: " + productReview.Rating + "Review: " + productReview.Review);
+            }
+        }
+    }
+}
