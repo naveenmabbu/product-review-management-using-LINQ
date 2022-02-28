8ffd4f81182d2839f7603463ada8fb0474c41701
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-02-28 23:17:26 +0530

    Add files via upload
    Add Get ProductId and Review of Records in Product Review List

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using ProductReviewManagementWithLinq;
+
+
+//UC1
+Console.WriteLine("Welcome to Product Review Management");
+///List of ProductDetails
+List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+                new ProductReview(){ProductID=1,UserID=2,Rating=6,Review="Average",isLike=true},
+                new ProductReview(){ProductID=2,UserID=5,Rating=6,Review="Good",isLike=false},
+                new ProductReview(){ProductID=3,UserID=10,Rating=6,Review="Bad",isLike=true},
+                new ProductReview(){ProductID=4,UserID=2,Rating=6,Review="Average",isLike=true},
+                new ProductReview(){ProductID=5,UserID=2,Rating=6,Review="Average",isLike=false},
+                new ProductReview(){ProductID=6,UserID=2,Rating=6,Review="Good",isLike=true},
+                new ProductReview(){ProductID=7,UserID=2,Rating=6,Review="Bad",isLike=false},
+                new ProductReview(){ProductID=8,UserID=2,Rating=6,Review="Bad",isLike=true},
+                new ProductReview(){ProductID=9,UserID=2,Rating=6,Review="Excellent",isLike=true},
+                new ProductReview(){ProductID=10,UserID=2,Rating=6,Review="Average",isLike=true},
+                new ProductReview(){ProductID=11,UserID=2,Rating=6,Review="Bad",isLike=false},
+                new ProductReview(){ProductID=12,UserID=2,Rating=6,Review="Bad",isLike=true},
+                new ProductReview(){ProductID=13,UserID=2,Rating=6,Review="Average",isLike=true},
+                new ProductReview(){ProductID=14,UserID=2,Rating=6,Review="Average",isLike=false},
+                new ProductReview(){ProductID=15,UserID=2,Rating=6,Review="Good",isLike=false},
+                new ProductReview(){ProductID=16,UserID=2,Rating=6,Review="Good",isLike=false},
+                new ProductReview(){ProductID=17,UserID=2,Rating=6,Review="Average",isLike=true},
+                new ProductReview(){ProductID=18,UserID=2,Rating=6,Review="Average",isLike=true},
+                new ProductReview(){ProductID=19,UserID=2,Rating=6,Review="Bad",isLike=false},
+                new ProductReview(){ProductID=20,UserID=2,Rating=6,Review="Good",isLike=true},
+                new ProductReview(){ProductID=21,UserID=2,Rating=6,Review="Good",isLike=false},
+                new ProductReview(){ProductID=22,UserID=2,Rating=6,Review="Bad",isLike=true},
+                new ProductReview(){ProductID=23,UserID=2,Rating=6,Review="Average",isLike=true},
+                new ProductReview(){ProductID=24,UserID=2,Rating=6,Review="Average",isLike=false},
+                new ProductReview(){ProductID=25,UserID=2,Rating=6,Review="Average",isLike=true}
+            };
+
+ProductReviewRepository productReview = new ProductReviewRepository();
+//uc2
+productReview.GetTopThreeProducts(productReviewList);
+//uc3
+productReview.RetrieveProductsForCondition(productReviewList);
+//uc4
+productReview.CountByProductID(productReviewList);
+//uc5
+productReview.GetProductIDAndReview(productReviewList);
+
+
+Console.ReadKey();
