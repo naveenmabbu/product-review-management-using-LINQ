9007f769e95c9ccaad00c36a376905018270b184
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-02-28 23:00:03 +0530

    Add Get Top Three Records based on rating

---
+++ Management.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagementWithLinq
+{
+    class ProductReviewRepository
+    {
+        /// <summary>
+        /// UC 2:
+        /// Returns the top three products with maximum rating
+        /// </summary>
+        /// <param name="productReviewList"></param>
+        public void GetTopThreeProducts(List<ProductReview> productReviewList)
+        {
+            var recordedData = (from products in productReviewList
+                                orderby products.Rating descending
+                                select products).Take(3);
+            foreach (var productReview in recordedData)
+            {
+                Console.WriteLine("Product ID: " + productReview.ProductID + " User ID: " + productReview.UserID + " Rating: " + productReview.Rating + " Review: " + productReview.Review);
+            }
+        }
+    }
+}
